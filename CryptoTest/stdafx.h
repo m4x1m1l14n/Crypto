0d0eaa9afd14740a3d7914ffd94bff176434a377
pac <sepental.patrik@gmail.com>
2019-02-21 15:26:46 +0100

    Added project for test purposes

---
+++ CryptoTest/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#pragma comment(lib, "Crypto.lib")
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <Crypto\Hex.hpp>
+#include <Crypto\Random.hpp>
+#include <Crypto\Hash.hpp>
+
+// TODO: reference additional headers your program requires here
